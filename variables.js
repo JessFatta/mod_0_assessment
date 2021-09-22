2c779396ba68d74b08fce5dfa5cb99bfb11d8312
Jessica Fatta <jessicafatta@gmail.com>
2021-09-22 19:59:00 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Casino";
+var isEntertaining = true;
+movie = "Godfather";
+var num4 = 11;
+var num6 = 2;
+var product = num4 * num6;
+var favoriteFoods = ["tacos", "pizza", "cucumbers"];
+favoriteFoods.shift()
+movie.length
