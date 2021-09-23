9b2cbf25c21a0e6397f50e26b77b6ceb29cb5a1c
Jessica Fatta <jessicafatta@gmail.com>
2021-09-22 20:07:25 -0400

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  school: "Turing School of Software & Design",
+  program: "Front End",
+  cohort: 2110,
+  currentMod: 0,
+  isConfident: true,
+  isScared: false,
+  tipsForSuccess: ["study hard", "take notes", "ask questions"],
+  selfCare: ["sleep well", "stay hydrated", "eat healthy", "pom breaks"],
+};
