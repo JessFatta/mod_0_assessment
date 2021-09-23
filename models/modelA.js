173ad014468c3aa020b0a5995fce0162f42f2142
Jessica Fatta <jessicafatta@gmail.com>
2021-09-22 20:04:08 -0400

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  species: "Tyrannosaurus Rex",
+  nickname: "T-Rex",
+  averageMPH: 17,
+  numberOfTeeth: 60,
+  isTerrifying: true,
+  isExtinct: true,
+  diet: ["Triceratops", "Edmontosaurus", "anything it wanted"],
+  anatomy: ["scales", "tiny arms", "huge mouth"],
+};
